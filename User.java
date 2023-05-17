bedeb3206b6a45e3712340a8d37802239f1e2275
NasiAyamBakar <abrar22082000@gmail.com>
2023-05-17 21:48:48 +0700

    menambahkan komen

--- User.java
+++ User.java
@@ -1,13 +1,15 @@
 public abstract class User {
-    private String nama;
+    private String nama; // Mendeklarasikan variabel instance privat 'nama' bertipe String
 
-    public User(String nama) {
+    public User(String nama) { // Konstruktor untuk menginisialisasi variabel 'nama'
         this.nama = nama;
     }
 
-    public String getNama() {
+    public String getNama() { // Metode Getter untuk mengambil nilai 'nama'
         return nama;
     }
 
-    public abstract void menu();
+    public abstract void menu(); 
+    // Metode 'menu' abstrak tanpa implementasi
+        // Metode 'menu' dinyatakan abstrak, yang artinya harus diimplementasikan oleh subkelas 'Pengguna'
 }
