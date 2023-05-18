96ada7c7c96d146fcd9b41e6edf9958b174e22c7
rajessssssss <rajasafattar@gmail.com>
2023-05-18 20:36:49 +0700

    menambahkan comment by rajasa

--- Mahasiswa.java
+++ Mahasiswa.java
@@ -1,32 +1,39 @@
+// Class Mahasiswa
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Mahasiswa {
+public class Mahasiswa { 
+    // Instance variables
     private String nim;
     private String nama;
     private int angkatan;
     private ArrayList<Float> daftarNilai = new ArrayList<>();
     private ArrayList<Integer> daftarBobot = new ArrayList<>();
 
+    // Constructor
     public Mahasiswa(String nim, String nama, int angkatan) {
         this.nim = nim;
         this.nama = nama;
         this.angkatan = angkatan;
     }
 
+    // Method to get NIM
     public String getNim() {
         return this.nim;
     }
 
+    // Method to get nama
     public String getNama() {
         return this.nama;
     }
 
+    // Method to set nilai and bobot
     public void setNilai(float nilai, int bobot) {
         this.daftarNilai.add(nilai);
         this.daftarBobot.add(bobot);
     }
 
+    // Method to calculate nilai rata-rata
     public float hitungNilai() {
         float total = 0;
         int totalBobot = 0;
@@ -44,6 +51,7 @@ public class Mahasiswa {
         return (total / totalBobot);
     }
 
+    // Method to display daftar nilai
     public void lihatDaftarNilai() {
         if (this.daftarNilai.isEmpty()) {
             System.out.println("Belum ada nilai yang dimasukkan.");
@@ -57,6 +65,7 @@ public class Mahasiswa {
         }
     }
 
+    // Static method to create new Mahasiswa object
     public static Mahasiswa tambahMahasiswa() {
         Scanner input = new Scanner(System.in);
 
